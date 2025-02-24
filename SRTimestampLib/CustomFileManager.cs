86bae3ba1675252d9e29d66ef548fa910db5f2d9
bookdude13 <rfredlund13@gmail.com>
2025-02-23 21:16:49 -0600

    Re-add includes needed on the Unity side

--- SRTimestampLib/CustomFileManager.cs
+++ SRTimestampLib/CustomFileManager.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SQLite;
